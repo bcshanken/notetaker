bd2d2164d2ebad5d4d626715215456547a4c68f1
Brian Shanken <bcshanken@gmail.com>
2020-11-17 19:32:41 -0500

    api calls working now

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // Dependencies
-const path = require("path");
-const http= require("http");
-const fs= require("fs");
-const express = require("express");
-const app = express();
-var PORT = process.env.PORT || 8080;
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+const { notStrictEqual } = require("assert");
+
+const { v4: uuidv4 } = require("uuid");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
 
 const notes = [{
     title: "first task",
@@ -14,7 +19,38 @@ const notes = [{
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
+// routes
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+// api calls
+app.get("/api/notes", function(req,res) {
+    storedNotes = fs.readFileSync("./db/db.json");
+    storedNotes = JSON.parse(storedNotes);
+    res.json(storedNotes);
+})
+
+app.post("/api/notes", function (req, res) {
+    storedNotes = fs.readFileSync("./db/db.json");
+    storedNotes = JSON.parse(storedNotes);
+    req.body.id = uuidv4();
+    storedNotes.push(req.body);
+    storedNotes = JSON.stringify(storedNotes);
+    fs.writeFileSync("./db/db.json", storedNotes);
+    storedNotes = JSON.parse(storedNotes);
+    res.json(storedNotes);
+  });
+
+
+// get the server going
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
 });
+
